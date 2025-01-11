4b89d95fbeffff5fc6515221e0c9636b66c599e3
Zelmoron <ak.igor13@mail.ru>
2025-01-11 22:47:07 +0800

    env

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "Tour/internal/app"
+import (
+	"Tour/internal/app"
 
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		logrus.Error("Error loading .env file")
+	}
+}
 func main() {
 	app := app.New()
 	app.Run()
